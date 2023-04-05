1cfc97817a14fca00f879f6d7be8b44d1ef5c026
Luke Gartner <lukegartner4@gmail.com>
2023-04-04 22:45:05 -0500

    Stretch Todo complete

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -83,16 +83,24 @@ console.log("My favorite foods are now", favoriteFoods);
 // 4.e (STRETCH) TODO: Replace the second food in your array
 //      with another one of your favorite foods.
 //      Then log the updated array.
+favoriteFoods[1] = "turkey";
+console.log("Replaced my second favorite food", favoriteFoods);
 
 // 4.f (STRETCH) TODO: Sort your favoriteFoods array
 //     in reverse alphabetical order. Log the array.
+favoriteFoods.sort().reverse();
+console.log(favoriteFoods);
 
 // 4.g (STRETCH) TODO: Convert your array to a string
 //     putting the word "and" between each item.
 //     eg "tacos and pizza and pasta". Log the string.
+let favoriteFoodsStr = favoriteFoods.join(" and ");
+console.log(favoriteFoodsStr);
 
 // 4.h (STRETCH) TODO: Make a new array that combines
 //     the favorite foods array with the animals array.
 //     Then log the new array.
 //     It should look something like:
 //     ['pizza', 'pasta', 'fish', 'cat', 'bird', 'dog']
+const foodAndAnimals = [...favoriteFoods, ...animalArray];
+console.log(foodAndAnimals);
